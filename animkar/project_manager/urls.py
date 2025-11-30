726e4d184162f7f6a8d370001a3aa57948be78f2
autocrat <autocratic.jalal@gmail.com>
2025-11-30 09:39:01 +0500

    create projects

---
+++ animkar/project_manager/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.ProjectListView.as_view(), name='project_list'),
+    path('create/', views.ProjectCreateView.as_view(), name='project_create'),
+    path('<int:pk>/update/', views.ProjectUpdateView.as_view(), name='project_update'),
+    path('<int:pk>/delete/', views.ProjectDeleteView.as_view(), name='project_delete'),
+]
--- animkar/project_manager/views.py
+++ animkar/project_manager/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Project
+from .forms import ProjectForm
 
-# Create your views here.
+class ProjectListView(ListView):
+    model = Project
+    template_name = 'project_manager/project_list.html'
+    context_object_name = 'projects'
+    ordering = ['-created_at']
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = ProjectForm()
+        return context
+
+class ProjectCreateView(CreateView):
+    model = Project
+    form_class = ProjectForm
+    template_name = 'project_manager/project_form.html'
+    success_url = reverse_lazy('project_list')
+
+class ProjectUpdateView(UpdateView):
+    model = Project
+    form_class = ProjectForm
+    template_name = 'project_manager/project_form.html'
+    success_url = reverse_lazy('project_list')
+
+class ProjectDeleteView(DeleteView):
+    model = Project
+    template_name = 'project_manager/project_confirm_delete.html'
+    success_url = reverse_lazy('project_list')
